27968500ca1f1937a677b81610587b1f69d5fafb
Brandon Hastings <65084955+brandon-hastings@users.noreply.github.com>
2025-02-25 15:20:13 +0100

    starting outline

--- sarek2tumourevo_samplesheet.R
+++ sarek2tumourevo_samplesheet.R
@@ -1,1 +1,11 @@
 
+get_sarek_output <- function(base_dir) {
+  
+}
+
+# take in list of parameters? IDK yet. Probably bad idea
+format_tumourevo_samplesheet <- function(params_list) {
+  tumourevo_samplesheet <- NULL
+  write.csv(tumourevo_samplesheet)
+  return(tumourevo_samplesheet)
+}
