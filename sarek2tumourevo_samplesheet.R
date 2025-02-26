da4bc9d7a0fc4bd734a35597ff14c0abb1c7c0f6
Brandon Hastings <65084955+brandon-hastings@users.noreply.github.com>
2025-02-26 17:24:43 +0100

    argument parser

--- sarek2tumourevo_samplesheet.R
+++ sarek2tumourevo_samplesheet.R
@@ -1,7 +1,26 @@
+source("getters_sarek.R")
 
-get_sarek_output <- function(base_dir) {
-  
+# parse args. Make more specific for optional args (coverage, purity, etc.)
+parse_args <- function() {
+  args = commandArgs(trailingOnly = TRUE)
+  if (length(args) != 2) {
+    if (dir.exists(args[1])) {
+      basedir <- args[1]
+      SPN_number <- args[2]
+    } else if (dir.exists(args[2])) {
+      basedir <- args[2]
+      SPN_number <- args[1]
+    } else {
+      stop("could not parse arguments or basedir does not exist")
+    }
+    else {
+      stop("Requires 2 arguments, the basedir and the SPN number")
+    }
+  }
+  return(list(basedir, SPN_number))
 }
+
+sarek_output <- batch_retrieve_sarek_variant_caller()
 
 # take in list of parameters? IDK yet. Probably bad idea
 format_tumourevo_samplesheet <- function(params_list) {
